cf657c5d26f6b33fbdde59665b8c0468c612018d
ShaiMaytav <shaimaytav01@gmail.com>
2023-07-12 15:10:38 +0300

    Update UserDataBase.h

--- CPPFinalProject/CPPFinalProject/UserDataBase.h
+++ CPPFinalProject/CPPFinalProject/UserDataBase.h
@@ -6,8 +6,13 @@
 class UserDatabase
 {
 public:	
+<<<<<<< Updated upstream
 	User* GetUser(std::string id);
 	void AddUser(User user);
+=======
+	User GetUser(std::string id);
+	void AddUser(User* user);
+>>>>>>> Stashed changes
 	bool RemoveUser(User* user);
 private:
 	std::vector<User*> users;
